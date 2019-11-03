42572ed1c7324e1d994336951e3ae92be867e2f8
Hadi Shad <hadi@mrhadi.com>
2019-11-04 10:00:39 +1100

    Update on pug template

--- src/utils/makeApp.js
+++ src/utils/makeApp.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const moment = require('moment')
+const moment = require('moment-timezone')
 
 const routes = require('../routes')
 
