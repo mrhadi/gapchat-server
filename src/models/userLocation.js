365c1e8ad6c345ddd63c61c3fb75c78e55538a59
Hadi Shad <hadi@mrhadi.com>
2019-11-03 20:48:59 +1100

    Update on location

--- src/models/userLocation.js
+++ src/models/userLocation.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose')
-const autopopulate = require('mongoose-autopopulate')
-
 const logger = require('../utils/logger')
 
 const Schema = mongoose.Schema
@@ -31,19 +29,12 @@ const schema = new Schema(
     },
     requestedBy: {
       type: String
-    },
-    lead: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-      autopopulate: true
     }
   },
   {
     timestamps: true
   }
 )
-
-schema.plugin(autopopulate)
 
 schema.index({ location: '2dsphere' })
 schema.index({ updatedAt: 1 }) // { expireAfterSeconds: 360 }
