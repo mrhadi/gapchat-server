ccd29ce2a15f4846fe4e855df3620b0e798578d6
Hadi Shad <hadi@mrhadi.com>
2019-11-03 13:34:48 +1100

    Update on location api

--- src/api/v1.0/locationAPI.js
+++ src/api/v1.0/locationAPI.js
@@ -11,7 +11,7 @@ router.post(
     check('latitude').isFloat(),
     check('longitude').isFloat(),
     check('speed').isFloat(),
-    check('source').isString()
+    check('source').isJSON()
   ],
   asyncHandler(async (req, res) => {
     const result = validationResult(req)
