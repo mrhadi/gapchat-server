112e6a6835f685eb0953b27e2ed5c027f607c640
Hadi Shad <hadi@mrhadi.com>
2019-11-06 21:49:30 +1100

    More updates

--- tools/bulk-users.js
+++ tools/bulk-users.js
@@ -1,29 +1,26 @@
 require('dotenv').config({ path: '../.env' })
-const util = require('util')
 
 const dbConnection = require('../src/db/dbConnection')
 const logger = require('../src/utils/logger')
 const City = require('../src/models/city')
 const User = require('../src/models/user')
 
-const fromCode = 1
-const toCode = 10
+const fromCode = 1001
+const toCode = 5000
 
 const addBulkUsers = async () => {
   for (let code = fromCode; code <= toCode; code++) {
     const city = await City.findOne({ code: code })
-    console.log(`${code} => ${city}`)
 
     let avatar = Math.floor(Math.random() * 110) + 1
     avatar = avatar < 10 ? `00${avatar}` : avatar < 100 ? `0${avatar}` : avatar
-    console.log(avatar)
 
     const user = new User({
       nickName: city.cityName,
       avatar: 'avatar_' + avatar,
       nearest: 1000,
       furthest: 20000,
-      type: 'CityCenter',
+      type: 'Bulk-CityCenter',
       deviceId:
         city.cityName.replace(' ', '') +
         '-' +
@@ -33,7 +30,9 @@ const addBulkUsers = async () => {
     })
     console.log(user)
 
-    // await user.save()
+    await user.save()
+
+    console.log(`=> ${code}`)
   }
 }
 
