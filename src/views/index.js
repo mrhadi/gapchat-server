69eb954dbf1fa9a771fb574fe6398b340d4a7cb9
Hadi Shad <hadi@mrhadi.com>
2019-11-07 22:14:20 +1100

    More updates

--- src/views/index.js
+++ src/views/index.js
@@ -24,7 +24,7 @@ router.get(
       }
     ])
 
-    const numUserLocation = UserLocation.estimatedDocumentCount()
+    const numUserLocation = UserLocation.count()
     console.log('numUserLocation', numUserLocation)
     res.render('userLocation', { userLocation, numUserLocation })
   })
