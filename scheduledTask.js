1a10c6889a9f61d7db08cc324e040d3fbbf15676
Hadi Shad <hadi@mrhadi.com>
2019-11-11 14:42:26 +1100

    More updates

--- scheduledTask.js
+++ scheduledTask.js
@@ -1,30 +1,89 @@
 require('dotenv').config()
 
-const axios = require('./src/axios')
+const dbConnection = require('./src/db/dbConnection')
 const logger = require('./src/utils/logger')
 
-const deleteInactiveLocations = async () => {
-  const res = await axios.get('/task/inactive-locations')
-  if (res && res.data) {
-    logger.log('deleteInactiveLocations:', res.data)
+const getWeather = require('./src/services/getWeather')
+
+const City = require('./src/models/city')
+const User = require('./src/models/user')
+const UserLocation = require('./src/models/userLocation')
+
+const addCityCenterLocation = async () => {
+  const updatedLocations = []
+  const randomUsers = await User.aggregate([
+    {
+      $match: {
+        type: 'Bulk-CityCenter'
+      }
+    },
+    {
+      $sample: {
+        size: 20
+      }
+    }
+  ])
+
+  for (const user of randomUsers) {
+    const location = await UserLocation.findOne({ deviceId: user.deviceId })
+    if (location) {
+      logger.log(`addCityCenterLocation: Ignoring ${user.nickName}`)
+      continue
+    }
+
+    const city = await City.findOne({
+      cityName: user.cityName,
+      countryCode: user.countryCode
+    })
+
+    if (city) {
+      const location = {
+        deviceId: user.deviceId,
+        speed: 0,
+        location: {
+          type: 'Point',
+          coordinates: [city.longitude, city.latitude]
+        },
+        requestedBy: 'FakeLocation'
+      }
+
+      const weather = await getWeather(city.latitude, city.longitude)
+      if (weather) {
+        location.weather = JSON.stringify(weather)
+      }
+
+      const userLocation = await UserLocation.findOneAndUpdate(
+        { deviceId: user.deviceId },
+        location,
+        { new: true, upsert: true, setDefaultsOnInsert: true }
+      )
+
+      updatedLocations.push(userLocation)
+    }
   }
+
+  return updatedLocations
 }
 
-const updateFakeLocations = async () => {
-  const res = await axios.get('/task/fake-location')
-  if (res && res.data) {
-    logger.log('updateFakeLocations: Done')
-  }
-}
+dbConnection.on('error', logger.error.bind(console, 'mongoose.connection:'))
+dbConnection.once('open', async () => {
+  logger.log('Scheduled tasks started ...')
 
-logger.log('Scheduled tasks started ...')
+  try {
+    logger.log('addCityCenterLocation started')
+    const res = await addCityCenterLocation()
+    logger.log(`addCityCenterLocation ended, ${res.length} locations added`)
+  } catch (err) {
+    logger.log('addCityCenterLocation:', err)
+  }
+
+  logger.log('Scheduled tasks ended')
+})
 
 /*
 ;(async () => {
   try {
-    await updateFakeLocations()
   } catch (error) {
-    logger.log(error)
   }
 })()
 */
--- src/api/index.js
+++ src/api/index.js
@@ -3,10 +3,8 @@ const { API_VER } = require('../utils/config')
 
 const userAPI = require(`./${API_VER}/userAPI`)
 const locationAPI = require(`./${API_VER}/locationAPI`)
-const taskAPI = require(`./${API_VER}/taskAPI`)
 
 router.use(`/${API_VER}/user`, userAPI)
 router.use(`/${API_VER}/location`, locationAPI)
-router.use(`/${API_VER}/task`, taskAPI)
 
 module.exports = router
